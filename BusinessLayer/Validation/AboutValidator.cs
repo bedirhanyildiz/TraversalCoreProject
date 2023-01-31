d50abb16e462e1c1b1f4bfb1f9d4bbb41a57275a
bedirhanyildiz <bedirhanyildiz94@gmail.com>
2023-01-31 16:49:19 +0300

    Add project files.

---
+++ BusinessLayer/Validation/AboutValidator.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Validation
+{
+    public class AboutValidator : AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Açıklama Kısmını Boş Geçemezsiniz");
+            RuleFor(x => x.Image1).NotEmpty().WithMessage("Lütfen Görsel Seçiniz");
+            RuleFor(x => x.Description).MinimumLength(50).WithMessage("Lütfen En Az 50 Karakterlik Açıklama Giriniz");
+        }
+    }
+}
