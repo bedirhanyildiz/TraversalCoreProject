bd7e3e7b89622fa5434a93dc583535c3aba9be96
bedirhanyildiz <bedirhanyildiz94@gmail.com>
2023-03-21 16:06:58 +0300

    Add_Comment

---
+++ DataAccessLayer/Migrations/20230321084549_mig_add_comment.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class migaddcomment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Destinations",
+                table: "Destinations");
+
+            migrationBuilder.RenameTable(
+                name: "Destinations",
+                newName: "Destination");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Destination",
+                table: "Destination",
+                column: "DestinationID");
+
+            migrationBuilder.CreateTable(
+                name: "Comment",
+                columns: table => new
+                {
+                    CommentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CommentUser = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CommentDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CommentContent = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CommentState = table.Column<bool>(type: "bit", nullable: false),
+                    DestinationID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comment", x => x.CommentId);
+                    table.ForeignKey(
+                        name: "FK_Comment_Destination_DestinationID",
+                        column: x => x.DestinationID,
+                        principalTable: "Destination",
+                        principalColumn: "DestinationID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_DestinationID",
+                table: "Comment",
+                column: "DestinationID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Destination",
+                table: "Destination");
+
+            migrationBuilder.RenameTable(
+                name: "Destination",
+                newName: "Destinations");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Destinations",
+                table: "Destinations",
+                column: "DestinationID");
+        }
+    }
+}
---
+++ DataAccessLayer/Migrations/20230321084920_mig_add_comment1.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class migaddcomment1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_Destination_DestinationID",
+                table: "Comment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Destination",
+                table: "Destination");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Comment",
+                table: "Comment");
+
+            migrationBuilder.RenameTable(
+                name: "Destination",
+                newName: "Destinations");
+
+            migrationBuilder.RenameTable(
+                name: "Comment",
+                newName: "Comments");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comment_DestinationID",
+                table: "Comments",
+                newName: "IX_Comments_DestinationID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Destinations",
+                table: "Destinations",
+                column: "DestinationID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Comments",
+                table: "Comments",
+                column: "CommentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Destinations_DestinationID",
+                table: "Comments",
+                column: "DestinationID",
+                principalTable: "Destinations",
+                principalColumn: "DestinationID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Destinations_DestinationID",
+                table: "Comments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Destinations",
+                table: "Destinations");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Comments",
+                table: "Comments");
+
+            migrationBuilder.RenameTable(
+                name: "Destinations",
+                newName: "Destination");
+
+            migrationBuilder.RenameTable(
+                name: "Comments",
+                newName: "Comment");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comments_DestinationID",
+                table: "Comment",
+                newName: "IX_Comment_DestinationID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Destination",
+                table: "Destination",
+                column: "DestinationID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Comment",
+                table: "Comment",
+                column: "CommentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_Destination_DestinationID",
+                table: "Comment",
+                column: "DestinationID",
+                principalTable: "Destination",
+                principalColumn: "DestinationID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
